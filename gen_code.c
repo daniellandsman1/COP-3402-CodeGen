4b85b461889ac420970f866caed3ae07be3fb662
Ryan Rohan <ryanrohan@Ryans-MBP.lan>
2024-11-28 23:51:22 -0500

    remove debug statements

--- gen_code.c
+++ gen_code.c
@@ -20,26 +20,22 @@
 // Initialize the code generator
 void gen_code_initialize()
 {
-    printf("Initializing the code generator...\n");
     literal_table_initialize();
 }
 
 // Write all instructions in code_seq `cs` to binary output file `bf`
 static void gen_code_output_seq(BOFFILE bf, code_seq cs)
 {
-    printf("Writing instructions to the binary file...\n");
     while (!code_seq_is_empty(cs))
     {
         bin_instr_t instr = (code_seq_first(cs))->instr;
         instruction_write_bin_instr(bf, instr);
         cs = code_seq_rest(cs);
     }
-    printf("Finished writing instructions.\n");
 }
 
 // Generate a BOF header for the program
 static BOFHeader gen_code_program_header(code_seq main_cs) {
-    printf("Generating program header...\n");
     BOFHeader header;
     bof_write_magic_to_header(&header); // Write magic using provided function
     header.text_start_address = 0;
@@ -50,153 +46,125 @@ static BOFHeader gen_code_program_header(code_seq main_cs) {
     header.data_length = literal_table_size();
     header.stack_bottom_addr = data_start + header.data_length + STACK_SPACE;
 
-    printf("Header generated: text_length=%d, data_start_address=%d, data_length=%d, stack_bottom_addr=%d\n",
-           header.text_length, header.data_start_address, header.data_length, header.stack_bottom_addr);
     return header;
 }
 
 // Output literals to the binary file
 static void gen_code_output_literals(BOFFILE bf)
 {
-    printf("Outputting literals...\n");
     literal_table_start_iteration();
     while (literal_table_iteration_has_next())
     {
         bof_write_word(bf, literal_table_iteration_next());
     }
     literal_table_end_iteration();
-    printf("Finished outputting literals.\n");
 }
 
 // Write the program to the binary file
 static void gen_code_output_program(BOFFILE bf, code_seq main_cs)
 {
-    printf("Starting program output...\n");
     BOFHeader header = gen_code_program_header(main_cs);
     bof_write_header(bf, header);
     gen_code_output_seq(bf, main_cs);
     gen_code_output_literals(bf);
-    printf("Program output complete.\n");
 }
 
 // Generate code for the program's block and output it
 void gen_code_program(BOFFILE bf, block_t prog) {
-    printf("Generating program code...\n");
 
     code_seq program_cs = code_seq_empty();
 
     // Set up the program
-    printf("Setting up the program...\n");
     code_seq setup_cs = code_utils_set_up_program();
     code_seq_concat(&program_cs, setup_cs);
 
     // Generate code for the program's block
-    printf("Generating code for the program's block...\n");
     code_seq block_cs = gen_code_block(prog);
     code_seq_concat(&program_cs, block_cs);
 
     // Tear down the program
-    printf("Tearing down the program...\n");
     code_seq teardown_cs = code_utils_tear_down_program();
     code_seq_concat(&program_cs, teardown_cs);
 
     // Output the program
     gen_code_output_program(bf, program_cs);
-    printf("Program generation complete.\n");
 }
 
 // Generate code for a block
 code_seq gen_code_block(block_t block) {
-    printf("Generating code for a block...\n");
     code_seq ret = code_seq_empty();
 
     // Handle empty blocks
     if (block.var_decls.var_decls == NULL && block.stmts.stmt_list.start == NULL &&
         block.const_decls.start == NULL) {  // Fix for `const_decls`
-        printf("Block is empty; generating EXIT instruction.\n");
         code *exit_instr = code_exit(0);
         code_seq_add_to_end(&ret, exit_instr);
         return ret;
     }
 
     // Save static link
-    printf("Saving static link...\n");
     code *store_static_link = code_swr(FP, SAVED_STATIC_LINK_OFFSET, FP);
     code_seq_add_to_end(&ret, store_static_link);
 
     // Save frame pointer
-    printf("Saving frame pointer...\n");
     code_seq save_fp_cs = code_utils_copy_regs(3, FP);
     code_seq_concat(&ret, save_fp_cs);
 
     // Save registers
-    printf("Saving registers...\n");
     code_seq save_cs = code_utils_save_registers_for_AR();
     code_seq_concat(&ret, save_cs);
 
     // Handle const declarations
     if (block.const_decls.start != NULL) {  // Fix for `const_decls`
-        printf("Generating code for const declarations...\n");
         code_seq const_decls_cs = gen_code_constDecls(block.const_decls);
         code_seq_concat(&ret, const_decls_cs);
     }
 
     // Generate variable declarations
-    printf("Generating variable declarations...\n");
     code_seq var_decls_cs = gen_code_var_decls(block.var_decls);
     code_seq_concat(&ret, var_decls_cs);
 
     // Generate statements
-    printf("Generating statements...\n");
     code_seq stmts_cs = gen_code_stmts(block.stmts);
     code_seq_concat(&ret, stmts_cs);
 
     // Restore registers
-    printf("Restoring registers...\n");
     code_seq restore_cs = code_utils_restore_registers_from_AR();
     code_seq_concat(&ret, restore_cs);
 
-    printf("Block generation complete.\n");
     return ret;
 }
 
 // Generate code for variable declarations
 code_seq gen_code_var_decls(var_decls_t vds)
 {
-    printf("Generating variable declarations...\n");
     code_seq ret = code_seq_empty();
     
     var_decl_t *vdp = vds.var_decls; // Start with the first variable declaration
     if (vdp == NULL) {
-        printf("No variable declarations found.\n");
         return ret; // No declarations, return an empty code sequence
     }
 
     while (vdp != NULL)
     {
-        printf("Generating code for a variable declaration...\n");
         code_seq new_var_decl = gen_code_var_decl(*vdp);
         code_seq_concat(&ret, new_var_decl);
         vdp = vdp->next; // Move to the next variable declaration
     }
 
-    printf("Finished generating variable declarations.\n");
     return ret;
 }
 
 // Generate code for a single variable declaration
 code_seq gen_code_var_decl(var_decl_t vd)
 {
-    printf("Generating code for a single variable declaration...\n");
     code_seq ret = gen_code_ident_list(vd.ident_list); // Generate code for the identifier list
-    printf("Finished generating code for a single variable declaration.\n");
     return ret;
 }
 
-// Updated gen_code_ident_list 
+// Generate code for an identifier list
 code_seq gen_code_ident_list(ident_list_t ident_list)
 {
-    printf("Generating code for an identifier list... New edition!\n");
     code_seq ret = code_seq_empty();
     ident_t *idp = ident_list.start; // Start with the first identifier
     
@@ -216,40 +184,6 @@ code_seq gen_code_ident_list(ident_list_t ident_list)
     }
     return ret;
 }
-
-/*
-// Generate code for an identifier list
-code_seq gen_code_ident_list(ident_list_t ident_list)
-{
-    printf("Generating code for an identifier list...\n");
-    code_seq ret = code_seq_empty();
-
-    ident_t *idp = ident_list.start; // Start with the first identifier
-    if (idp == NULL) {
-        printf("No identifiers found in the list.\n");
-        return ret; // Return empty sequence if no identifiers
-    }
-
-    while (idp != NULL)
-    {
-        if (idp->name == NULL) {
-            printf("Error: Identifier name is NULL.\n");
-            bail_with_error("Invalid identifier in list: name is NULL.");
-        }
-
-        printf("Generating code for identifier: %s\n", idp->name);
-
-        // Generate code for the current identifier
-        code_seq new_ident = gen_code_ident(*idp);
-        code_seq_concat(&ret, new_ident);
-
-        idp = idp->next; // Move to the next identifier
-    }
-
-    printf("Finished generating code for an identifier list.\n");
-    return ret;
-}
-*/
 
 // Generate code for a statement
 code_seq gen_code_stmt(stmt_t stmt)
@@ -298,10 +232,8 @@ code_seq gen_code_assign_stmt(assign_stmt_t stmt)
 // Generate code for a list of statements
 code_seq gen_code_stmts(stmts_t stmts)
 {
-    printf("Generating code for statements...\n");
     if (stmts.stmts_kind == empty_stmts_e)
     {
-        printf("Statements are empty; generating NOP.\n");
         return code_seq_singleton(code_nop());
     }
 
@@ -309,12 +241,10 @@ code_seq gen_code_stmts(stmts_t stmts)
     stmt_t *sp = stmts.stmt_list.start;
     while (sp != NULL)
     {
-        printf("Generating code for a statement...\n");
         code_seq stmt_cs = gen_code_stmt(*sp);
         code_seq_concat(&ret, stmt_cs);
         sp = sp->next;
     }
-    printf("Finished generating code for statements.\n");
     return ret;
 }
 
@@ -322,79 +252,6 @@ code_seq gen_code_stmts(stmts_t stmts)
 code_seq gen_code_begin_stmt(block_stmt_t stmt) {
     return gen_code_stmts(stmt.block->stmts);
 }
-
-/*
-// Static counter for generating unique labels
-static unsigned int label_counter = 0;
-
-// Replicate logic for code_utils_new_label
-static unsigned int gen_code_new_label() {
-    return label_counter++;
-}
-*/
-
-/*
-// Replicate logic for code_label using jump
-static code* gen_code_label(unsigned int label) {
-    code* label_code = (code*)malloc(sizeof(code));
-    if (!label_code) {
-        bail_with_error("Memory allocation failed for label_code");
-    }
-
-    // Create a jump instruction with the label as the address
-    bin_instr_t instr;
-    instr.jump.op = JMPA_O; // Use the opcode for an absolute jump
-    instr.jump.addr = label; // Set the address to the label number
-
-    label_code->instr = instr;
-    label_code->next = NULL;
-
-    return label_code;
-}
-*/
-
-/*
-// Generate code for the IF statement
-code_seq gen_code_if_stmt(if_stmt_t stmt) {
-    code_seq ret = code_seq_empty();
-
-    // Generate code for the condition
-    code_seq cond_cs = gen_code_condition(stmt.condition);
-    code_seq_concat(&ret, cond_cs);
-
-    // Reserve labels for branching
-    unsigned int else_label = gen_code_new_label();
-    unsigned int end_label = stmt.else_stmts ? gen_code_new_label() : else_label;
-
-    // Jump to else if condition is false
-    code *branch_false = code_bne(3, 0, else_label); // 3 = result reg
-    code_seq_add_to_end(&ret, branch_false);
-
-    // Generate code for the "then" branch
-    code_seq then_cs = gen_code_stmts(*stmt.then_stmts);
-    code_seq_concat(&ret, then_cs);
-
-    // If there is an else branch, jump to the end after executing "then"
-    if (stmt.else_stmts) {
-        code *jump_end = code_jmpa(end_label);
-        code_seq_add_to_end(&ret, jump_end);
-    }
-
-    // Insert the "else" branch label
-    code_seq_add_to_end(&ret, gen_code_label(else_label));
-
-    // Generate code for the "else" branch if it exists
-    if (stmt.else_stmts) {
-        code_seq else_cs = gen_code_stmts(*stmt.else_stmts);
-        code_seq_concat(&ret, else_cs);
-
-        // Insert the end label
-        code_seq_add_to_end(&ret, gen_code_label(end_label));
-    }
-
-    return ret;
-}
-*/
 
 // Generate code for the IF statement (offset instead of label)
 code_seq gen_code_if_stmt(if_stmt_t stmt) {
@@ -405,8 +262,6 @@ code_seq gen_code_if_stmt(if_stmt_t stmt) {
     code_seq cond_cs = gen_code_condition(stmt.condition);
     code_seq_concat(&ret, cond_cs);
     
-    printf("DEBUG: DOING IF CONDITION!\n");
-
     // The result of the condition is now on the stack: 0 (false) or 1 (true)
     // We want to jump to the "else" block if false, or proceed with "then" if true
     // Calculate the offsets for conditional branches
@@ -720,23 +575,16 @@ code_seq gen_code_rel_op(token_t rel_op)
 // Generate code for an identifier
 code_seq gen_code_ident(ident_t id)
 {
-    printf("Generating code for identifier: %s\n", id.name ? id.name : "(null)");
-
     code_seq ret = code_seq_empty();
 
     if (id.idu == NULL) {
-        printf("Error: id_use (idu) is NULL for identifier '%s'.\n", id.name);
         bail_with_error("Invalid identifier: id_use is NULL.");
     }
 
     lexical_address *addr = id_use_2_lexical_address(id.idu);
     if (addr == NULL) {
-        printf("Error: Failed to compute lexical address for identifier '%s'.\n", id.name);
         bail_with_error("Invalid lexical address.");
     }
-
-    printf("Lexical address for '%s': levelsOutward=%u, offsetInAR=%u\n",
-           id.name, addr->levelsOutward, addr->offsetInAR);
 
     code *load_var = code_lwr(addr->levelsOutward, addr->offsetInAR, 0);
     if (load_var == NULL) {
@@ -769,7 +617,6 @@ code_seq gen_code_number(number_t num)
 
 /// Generate code for const declarations list
 code_seq gen_code_constDecls(const_decls_t cds) {
-    printf("Generating code for const declarations list...\n");
     code_seq ret = code_seq_empty();
 
     const_decl_t *cd = cds.start;  // Fix for `start`
@@ -779,26 +626,22 @@ code_seq gen_code_constDecls(const_decls_t cds) {
         cd = cd->next;  // Traverse to the next const declaration
     }
 
-    printf("Finished generating code for const declarations list.\n");
     return ret;
 }
 
 // Generate code for a single const declaration
 code_seq gen_code_constDecl(const_decl_t cd) {
-    printf("Generating code for a const declaration...\n");
     code_seq ret = code_seq_empty();
 
     // Handle the constant definition list
     code_seq const_def_list_cs = gen_code_const_def_list(cd.const_def_list);
     code_seq_concat(&ret, const_def_list_cs);
 
-    printf("Finished generating code for a const declaration.\n");
     return ret;
 }
 
 // Generate code for const definitions list
 code_seq gen_code_const_def_list(const_def_list_t cdl) {
-    printf("Generating code for const definitions list...\n");
     code_seq ret = code_seq_empty();
 
     const_def_t *cdf = cdl.start;  // Fix for `start`
@@ -808,13 +651,11 @@ code_seq gen_code_const_def_list(const_def_list_t cdl) {
         cdf = cdf->next;  // Traverse to the next const definition
     }
 
-    printf("Finished generating code for const definitions list.\n");
     return ret;
 }
 
 // Generate code for a single const definition
 code_seq gen_code_constDef(const_def_t cdf) {
-    printf("Generating code for a const definition...\n");
     code_seq ret = code_seq_empty();
 
     // Add the constant value to the literal table
@@ -824,6 +665,5 @@ code_seq gen_code_constDef(const_def_t cdf) {
     code *load_const = code_cpw(SP, 0, GP, offset);
     code_seq_add_to_end(&ret, load_const);
 
-    printf("Finished generating code for a const definition.\n");
     return ret;
 }
